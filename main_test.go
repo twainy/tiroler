13a9060317a01bf9b4b22dc1ccd49d43c4bfe05e
twainy <wakasugi@gmail.com>
2015-03-14 22:27:47 +0900

    change field to public

--- main_test.go
+++ main_test.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"testing"
+	"encoding/json"
 )
 
 func TestMain(t *testing.T) {
-	mapA := map[string]string{"aaa": "bbbb", "ccc": "ddd"}
+	mapA := map[string]string{"aaa":"bbbb","ccc":"ddd"}
 	mapB, _ := json.Marshal(mapA)
+	if string(mapB) != "{\"aaa\":\"bbbb\",\"ccc\":\"ddd\"}" {
+        t.Error("json marshal error")
+        
+    }
+    
 }
